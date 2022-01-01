3063e2772a8b76c3deda81a71a60fbdfab041101
farpetrad <farpetrad@gmail.com>
2022-01-01 08:45:53 -0800

    Add Teams

--- SplinterLandsAPI/Models/BattleActionState.cs
+++ SplinterLandsAPI/Models/BattleActionState.cs
@@ -5,6 +5,7 @@
         public bool Alive { get; set; } = false;
         public List<int> Stats { get; set; } = new List<int>();
         public int Base_Health { get; set; } = -1;
+        //TODO: we need to flatten this to a key value pair instead of a List of Lists
         public List<List<string>> Other { get; set; } = new List<List<string>>(); 
     }
 }
---
+++ SplinterLandsAPI/Models/Team.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace SplinterLandsAPI.Models
+{
+    public class Team
+    {
+        public string Player { get; set; } = string.Empty;
+        public Int64 Rating { get; set; } = -1;
+        public string Color { get; set; } = string.Empty;
+        public JObject Summoner { get; set; } = new JObject();
+        public List<JObject> Monsters { get; set; } = new List<JObject>();
+    }
+}

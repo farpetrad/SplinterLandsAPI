08656a4a55fe963488b8fec9506e1a8437ea2acb
farpetrad <farpetrad@gmail.com>
2022-02-13 11:16:52 -0800

    #patch

--- SplinterLands.DTOs/Models/Card.cs
+++ SplinterLands.DTOs/Models/Card.cs
@@ -14,10 +14,10 @@ namespace SplinterLands.DTOs.Models
         public bool Is_Starter { get; set; } = false;
 
         public CardStats Stats { get; set; } = new CardStats();
+        public List<Distribution> Distribution { get; set; } = new List<Distribution>();
         public Object? Subtype { get; set; } = null;
         public Object? tier { get; set; } = null;
         public int MaxLevel => MaxLevelForRarity();
-
         private int MaxLevelForRarity()
         {
             if (Rarity == CardRarityEnum.Common)
@@ -38,5 +38,6 @@ namespace SplinterLands.DTOs.Models
             }
             return -1;
         }
+
     }
 }
